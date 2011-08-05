a5d43ce8c3f1c2d0671d0ef0e2bcaeca52153f82
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-08-05 18:40:12 +0200

    build opencv 2.3 only on master

--- init.rb
+++
@@ -1,5 +0,0 @@
-# In recent enough autoproj versions, declare that we want to use opencv from
-# source and not from OS
-if Autoproj.respond_to?(:add_osdeps_overrides)
-    Autoproj.add_osdeps_overrides 'opencv', :package => 'external/opencv', :force => true
-end
