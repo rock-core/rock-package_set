957579d0a2959cbb5f33426b4b2a7b5fc4639c68
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-12-12 10:06:25 +0100

    make bundles/rock part of the default install

--- overrides.rb
+++ overrides.rb
@@ -1,3 +1,4 @@
 if rock_toolchain = Autoproj.manifest.find_metapackage('rock.toolchain')
     rock_toolchain.add Autobuild::Package['gui/vizkit']
+    rock_toolchain.add Autobuild::Package['bundles/rock']
 end
