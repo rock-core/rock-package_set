5f70ed9a97b001bb74685ef952a6b6d6616aefbe
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-03-12 14:00:39 +0100

    added temporary fix for boost bug on debian testing
    probably resolved with next boost release, affected: 1.53
    
    https://svn.boost.org/trac/boost/ticket/7979

--- overrides.rb
+++ overrides.rb
@@ -2,3 +2,13 @@ if rock_toolchain = Autoproj.manifest.find_metapackage('rock.toolchain')
     rock_toolchain.add Autobuild::Package['gui/vizkit']
     rock_toolchain.add Autobuild::Package['bundles/rock']
 end
+
+
+# 2014-03-12:
+# temporary fix for boost bug: https://svn.boost.org/trac/boost/ticket/7979
+# on debian testing
+only_on 'debian' do
+  cmake_package 'typelib' do |pkg|
+      pkg.define "GLIBC_HAVE_LONG_LONG"
+  end  
+end
